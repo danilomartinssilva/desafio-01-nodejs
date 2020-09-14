e0bab94eabbfeed9d754b2c9b82cae28a145cb7e
Danilo Martins <danilomartins@MacBook-Pro-de-Danilo.local>
2020-09-14 20:13:48 -0300

    Nivel Repositório e Likes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require("uuid");
 
 const app = express();
 
@@ -11,23 +11,55 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const id = uuid();
+  const repository = { title, url, techs, id, likes: 0 };
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const { id } = request.params;
+  const repository = repositories.find((re) => re.id === id);
+  if (!repository) {
+    return response.status(400).send();
+  }
+
+  repository.title = title;
+  repository.url = url;
+  repository.techs = techs;
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repository = repositories.findIndex((rep, index) => rep.id === id);
+
+  if (repository !== -1) {
+    repositories.splice(repository, 1);
+  } else {
+    response.status(400).json({ error: "Repositório não existe" });
+  }
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repository = repositories.find((rep) => rep.id === id);
+
+  if (!repository) {
+    return response.status(400).send();
+  }
+
+  repository.likes += 1;
+
+  return response.json(repository);
 });
 
 module.exports = app;
